2ea0cd3c43015b97e768267c4531018c393027c8
abdulwahidgul24085 <abdulwahidgul@gmail.com>
2023-03-10 20:59:15 +0500

    django updated

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,12 +1,14 @@
+import os
+
 from core.settings.common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-be@_-7pmv^54#r&1jtd09=suj1$j2apusyi%9i#)1sraioh)up'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOSTS'),]
 
 INSTALLED_APPS += [
     "debug_toolbar",
@@ -15,12 +17,12 @@ INSTALLED_APPS += [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "ksu",
-        "USER": "awg",
-        "PASSWORD": "pass",
-        "HOST": "localhost",
-        "PORT": "2345",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
         'ATOMIC_REQUESTS': True,
     },
 }
